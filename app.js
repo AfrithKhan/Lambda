2ab7edab086c4fa127209e2020bcb17a55602b92
root <root@ip-172-31-45-233.ap-south-1.compute.internal>
2022-10-03 11:11:08 +0000

    builspec.yaml created

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const sls = require('serverless-http')
 const app = express()
 app.get('/', async (req, res, next) => {
-  res.status(200).send('Hello Afrith I am changed new')
+  res.status(200).send('Hello Afrith I am changed by code pipeline')
 })
 module.exports.server = sls(app)
