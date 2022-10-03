726b299c27ca47edc5366ddf1992f78084733d46
root <root@ip-172-31-45-233.ap-south-1.compute.internal>
2022-10-03 16:40:27 +0000

    lambda deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const sls = require('serverless-http')
 const app = express()
-app.get('/', async (req, res, next) => {
+app.get('/afrith', async (req, res, next) => {
   res.status(200).send('Hello Afrith I am changed by code pipeline again')
 })
 module.exports.server = sls(app)
